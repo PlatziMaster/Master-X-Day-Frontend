aa9c5bf14b152ae50a0d8eccc5a61085f8e248d9
Felipe Duitama <felipe.duitama@3dves.com>
2021-03-06 16:33:49 -0500

    add consume members data

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
 const URL = 'https://api.trello.com';
+const key = '66050e2c572e07f079166db1ae85e79d';
+const token = '79bfd2df83f0384425e8768165bd3f39bc8e5ba79eabcf8c0ba42ec49076045c';
 
 export const testAPI = async () => {
   try {
-    const result = await axios.get(`${URL}/1/boards/6043bc7c1097aa4c08408b1d?key=66050e2c572e07f079166db1ae85e79d&token=79bfd2df83f0384425e8768165bd3f39bc8e5ba79eabcf8c0ba42ec49076045c`);
+    const result = await axios.get(`${URL}/1/boards/6043bc7c1097aa4c08408b1d?key=${key}&token=${token}`);
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getMembers = async () => {
+  try {
+    const result = await axios.get(`${URL}/1/boards/6043bc7c1097aa4c08408b1d?key=${key}&token=${token}`);
     return result;
   } catch (error) {
     return error;
--- src/components/team/Team.ts
+++ src/components/team/Team.ts
@@ -1,6 +1,22 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
 import Member from '../member'
-export class Team extends Vue {
+import {getMembers} from '../../api'
 
-    
+@Component({
+    components: {},
+    props: {
+      name: String,
+      image: String,
+    },
+  })
+
+export class Team extends Vue {
+members = '';
+created() {
+    this.$nextTick(async () => {
+      this.members = await getMembers();
+    });
+  }
 
 }

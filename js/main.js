559ba336e8a28fc171502272143fcf1f99eb8b31
cexperto <rattman.c@gmail.com>
2021-03-06 18:39:43 -0500

    Commit using scss

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const urlBoard = 'https://api.trello.com/1/boards/604396bd14073051b7c38da3?key=1
 const urlcards = 'https://api.trello.com/1/cards/6043bb4102cc60148dbeb8e0?key=1a2d88f8c99ace7c5391697413220037&token=c719c5ceb460648640d2fba3d1e356be33e30cbcd3a523bc12ddbcf4824c3b44'
 
 
- /** Get que trae las listas de Trello **/
+ /** Get -> that provide us the lists from Trello **/
 fetch(urlList)
 .then(response => response.json())
 .then (data =>{
@@ -30,8 +30,7 @@ fetch(urlList)
 .catch(err=>console.log(err))
 
 
-
-/** Get que trae los tabletos de Trello**/
+/** Get -> that provide us the boards from Trello**/
 fetch(urlBoard)
 .then(response => response.json())
 .then (data =>{
@@ -45,11 +44,10 @@ fetch(urlBoard)
 .catch(err=>console.log(err))
 
 
-/** Get que trae los cards de Trello **/
+/** Get -> that provide us the cards from Trello **/
 fetch(urlcards)
 .then(response => response.json())
 .then (data =>{
-
     let element = document.getElementById('prueba')
     element.innerHTML = `
         <p>${data.name}</p>
@@ -57,15 +55,3 @@ fetch(urlcards)
     console.log(data)
 })
 .catch(err=>console.log(err))
-
-
-
-
-
-
-
-
-
-
-
-

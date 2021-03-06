fae8c2904132c01afa44e14e613484ff798cf368
Marcos Arroyo <marcosarroyog@gmail.com>
2021-03-06 17:40:56 -0600

    refactor: deleting demo classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
     <NavBar />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import NavBar from "@/components/MTNavBar.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     NavBar
   }
 };

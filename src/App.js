b61c03aa84f4d456b9662e9c74b4ee3d296e9d58
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-03-06 12:18:01 -0600

    project file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/scss/global.scss'
+import Header from './components/header'
 
 function App() {
   return (
     <div className="container">
-      <h2 className='text-center'>Captura</h2>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './scss/header.scss'
+
+export default function Header(props) {
+    return (
+        <div className='container'>
+            <h2 className='text-center'>Captura</h2>
+        </div>
+    )
+}

409ed5899d31976d416098b53ecf5658d0eaa72d
Adrian Bautista <adrianbautistaorozco@gmail.com>
2021-03-06 13:35:03 -0500

    Header Structure added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header'
 
 function App() {
   return (
-    <div className="container">
+    <div className="App">
       <Header/>
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './scss/header.scss'
+import Logo from '../assets/images/logo-trello.png'
 
 export default function Header(props) {
     return (
-        <div className='container'>
-            <h2 className='text-center'>Captura</h2>
-        </div>
+        <header className="header">
+            <nav className="navbar navbar-light justify-content-center">
+                <a className="navbar-brand" href="#">
+                <img src={Logo} alt="Logo Trello" />
+                </a>
+            </nav>
+        </header>
     )
 }

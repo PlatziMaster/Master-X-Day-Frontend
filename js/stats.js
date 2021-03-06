606b4b87aa481ccb8ace3ac43200f92203e3b70e
Ignacio Garza <ignacioagarza@gmail.com>
2021-03-06 17:10:06 -0600

    table with overall information

--- js/stats.js
+++ js/stats.js
@@ -1,68 +1,60 @@
 const labels = ["To do", "In progress", "Completed"];
 const dataPie = [4,4,2];
 const dataBar = [4,4,2];
+const tableInfo = document.getElementById("dataTable");
 
 
+drawCharts();
 
-const pieChart = new Chart(document.getElementById("pie-chart"), {
-type: 'pie',
-data: {
-labels: labels,
-datasets: [{
-  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-  data: dataPie
-}]
-},
-options: {
-title: {
-  display: true,
-  text: 'Percentage progress per state'
-}
-}
-});
+function drawCharts(){
 
-const barChart = new Chart(document.getElementById("bar-chart"), {
-    type: 'horizontalBar',
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
-          data: [6,4,2,0]
+    const pieChart = new Chart(document.getElementById("pie-chart"), {
+        type: 'pie',
+        data: {
+        labels: labels,
+        datasets: [{
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: dataPie
+        }]
+        },
+        options: {
+        title: {
+          display: true,
+          text: 'Percentage progress per state'
         }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Cards per list'
-      }
-    }
-});
+        }
+        });
+        
+        const barChart = new Chart(document.getElementById("bar-chart"), {
+            type: 'horizontalBar',
+            data: {
+              labels: labels,
+              datasets: [
+                {
+                  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+                  data: [6,4,2,0]
+                }
+              ]
+            },
+            options: {
+              legend: { display: false },
+              title: {
+                display: true,
+                text: 'Cards per list'
+              }
+            }
+        });
+        
+
+        drawTable();
+}
 
 
-/// Radar chart
-// const radarChart = new Chart(document.getElementById("radar-chart"), {
-//     type: 'radar',
-//     data: {
-//       labels: labels,
-//       datasets: [
-//         {
-//           label: "1950",
-//           fill: true,
-//           backgroundColor: "rgba(179,181,198,0.2)",
-//           borderColor: "rgba(179,181,198,1)",
-//           pointBorderColor: "#fff",
-//           pointBackgroundColor: "rgba(179,181,198,1)",
-//           data: [8.77,55.61,21.69,6.62,6.82]
-//         }
-//       ]
-//     },
-//     options: {
-//       title: {
-//         display: true,
-//         text: 'Distribution in % of world population'
-//       }
-//     }
-// });
+function drawTable(){
+    tableInfo.innerHTML = `
+    
+    `
+}
+
+
+

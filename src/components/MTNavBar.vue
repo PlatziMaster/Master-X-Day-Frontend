c2ecb1903bbd7dcbd63ed9b767e65cd2aa8df941
Javier Collazos <jcollazosgomez@gmail.com>
2021-03-06 18:58:23 -0500

    fix(nav): hidding drello name in small breakpoint

--- src/components/MTNavBar.vue
+++ src/components/MTNavBar.vue
@@ -1,10 +1,23 @@
 <template>
   <nav class="bg-MTBlack flex flex-row h-14 justify-between	">
     <div class="align-middle flex flex-wrap content-center h-full pl-6">
-      <img id="MTlogo" src="https://i.imgur.com/OT8gRC0.png" alt="Drello" class="h-10" />
-      <h1 class="text-white font-black text-lg pl-3 align-middle my-auto">Drello</h1>
+      <router-link to="/">
+        <img
+          id="MTlogo"
+          src="https://i.imgur.com/OT8gRC0.png"
+          alt="Drello"
+          class="h-10"
+        />
+      </router-link>
+      <h1
+        class="text-white font-black text-lg pl-3 align-middle my-auto hidden sm:block"
+      >
+        Drello
+      </h1>
     </div>
-    <ul class="none text-white font-medium align-middle flex flex-wrap gap-x-6 content-center pr-6">
+    <ul
+      class="none text-white font-medium align-middle flex flex-wrap gap-x-6 content-center pr-6"
+    >
       <li>
         <router-link to="/">Home</router-link>
       </li>

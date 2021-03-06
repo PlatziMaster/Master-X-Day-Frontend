fe958d9181fe182a005e70fe3990f4834473c128
ithzavg <ithgarvi@gmail.com>
2021-03-06 16:42:15 -0600

    Mockup

--- app_team_12/src/App.js
+++ app_team_12/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import user from './assets/001-user.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+
+      <div className="container">
+        <h2>Dashboard</h2>
+
+        <div className="row">
+          <div className="col-md-12">
+            <h3>Members</h3>
+            <ul className="list-user">
+              <li>
+                <img src={user} alt="User" className="img-user"></img>
+                <label>Daniela Vargas</label>
+              </li>
+              <li>
+                <img src={user} alt="User" className="img-user"></img>
+                <label>Nestor David Galeana</label>
+              </li>
+              <li>
+                <img src={user} alt="User" className="img-user"></img>
+                <label>Ithzamary Vilchis</label>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row"> 
+          <div className="col-offset-3"></div>
+          <h2>Total progress</h2>
+        </div>
+
+        <div className="row">
+          <div className="total-container">
+              
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <h3>To do</h3>
+            <div className="total-todo">
+
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h3>In Progress</h3>
+            <div className="total-progress">
+              
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h3>Done</h3>
+            <div className="total-done">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

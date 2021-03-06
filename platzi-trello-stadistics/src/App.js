6309aa821712eae685be4e9fd3676fffc7e7e75f
Sergio Gonzalez <sergioleonardo.gonzalez@gmail.com>
2021-03-06 18:00:56 -0500

    Fetch to Trello app.

--- platzi-trello-stadistics/src/App.js
+++ platzi-trello-stadistics/src/App.js
@@ -6,7 +6,17 @@ import Header from "./components/Header";
 import PieChart from "./components/PieChart";
 
 const App = () => {
-  return (
+
+    const getList = () => {
+        fetch("https://api.trello.com/1/boards/6043df7d663af602cf77c881?key=3ecdfc55093cc26752e7056b0834e1e4&token=57a87b374c6043be18dbc60f00962ba3a650c731b54df7f86b83e967becf22ec")
+            .then((res)=>res.ok? Promise.resolve(res) : Promise.reject(res))
+            .then((res)=>res.json())
+            .then((res) => console.log(res))
+    }
+
+    getList();
+
+    return (
     
       <div className="Dashboard">
         <div className="Dashboard__header">

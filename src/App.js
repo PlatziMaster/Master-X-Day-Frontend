34df6f54c707703ecb31be0d7178a216ff3fc8fb
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-03-06 11:47:32 -0600

    initialize project with react, bootstrap, scss ``

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/scss/global.scss'
+
+function App() {
+  return (
+    <div className="container">
+      <h2 className='text-center'>Captura</h2>
+    </div>
+  );
+}
+
+export default App;

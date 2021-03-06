a00db8f6f635d512637d0e916d85632bc3ec3305
davidnherran <davidnherran@gmail.com>
2021-03-06 17:38:20 -0500

    ok

--- trello-clon/src/App.js
+++ trello-clon/src/App.js
@@ -1,20 +1,18 @@
 import Card from './components/card'
-
 import menu from './icons/app-menu.svg'
 import home from './icons/home.svg'
 import table from './icons/table.svg'
 import dashb from './icons/dashb.svg'
 import star from './icons/star.svg'
 import lock from './icons/lock.svg'
-
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">{/*Dont delete*/}
-      <div className="navbar bg-primary">
+      <div className="navbar bg-primary" >
         <div className="container-fluid">
-          <button className="btn btn-light"><img src={menu} alt="Menu Icon"/></button>
+          <button className="btn btn-light aa"><img src={menu} alt="Menu Icon"/></button>
           <button className="btn btn-light"><img src={home} alt="Home Icon"/></button>
           <button className="btn btn-light"><img src={table} alt="Table Icon"/></button>
           </div>
@@ -22,7 +20,7 @@ function App() {
       <div className="container-fluid">
         <div className="navbar">
           <button className="btn btn-light"><img src={dashb} alt="Dashboard Icon"/>Board</button>
-          <h3>Board Title</h3>
+          <h3 className="aa">Board Titles</h3>
           <button className="btn btn-light"><img src={star} alt="Star Icon"/></button>
           <button className="btn btn-light"><img src={lock} alt="Lock Icon"/>Private</button>
           <button className="btn btn-light"><h4>Invite</h4></button>

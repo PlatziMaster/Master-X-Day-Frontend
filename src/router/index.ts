16180f1e08b83d6a9b84178c5979bd9a74a335c4
Yaf98 <manuelyg2097@gmail.com>
2021-03-06 14:48:15 -0600

    Add card component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,18 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/card',
+    name: 'Card',
+    component: () => import('../components/card'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home'),
   },
 ];
 

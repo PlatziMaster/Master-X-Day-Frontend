dc7bbf689249a089eb414ac396ef311d8f5827c6
Danilo Morales <darmito10@gmail.com>
2021-03-06 15:56:12 -0500

    feature cards

--- platzi-trello-stadistics/src/App.js
+++ platzi-trello-stadistics/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import CardList from './components/CardList'
+import CardPercentage from './components/CardPercentage'
 import "./styles/App.css"
 
 const App = () => {
   return (
     <div>
       Home
+
+
+      <CardList  />
+      <CardPercentage />
     </div>
   )
 }
--- platzi-trello-stadistics/src/components/CardPercentage.js
+++ platzi-trello-stadistics/src/components/CardPercentage.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import "../styles/box.css"
 const CardPercentage = () => {
     return (
-        <div>
-            % cartas
+        <div className="containerBox">
+        <div className="Box blue">
+            <small className="Box__head">Completed vs Total</small>
+            <p className="Box__Number">50%</p>
         </div>
+ 
+        <div className="Box green">
+            <small className="Box__head">In Progress vs Total</small>
+            <p className="Box__Number">50%</p>
+        </div>
+        
+    </div>
     )
 }
 

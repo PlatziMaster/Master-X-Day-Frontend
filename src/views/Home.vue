2bffbab994b2c3ec3109b63aa9554c95af2cd27b
IramML <irammartinezlamas@gmail.com>
2021-03-06 17:07:28 -0600

    Add tabs layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class='home'>
-    <img alt='Vue logo' src='../assets/logo.png' />
+  <div class="home container">
+    <ul class="nav nav-tabs nav-fill">
+      <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#board">Board</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#graphics">Graphics</a>
+      </li>
+    </ul>
+
+    <div class="tab-content">
+      <div id="board" class="tab-pane fade in active show">
+        <!-- Board content -->
+      </div>
+      <div id="graphics" class="tab-pane fade">
+         <!-- Graphics content -->
+      </div>
+    </div>
+    <!--  -->
   </div>
 </template>
 
@@ -20,3 +37,12 @@ export default class Home extends Vue {
   }
 }
 </script>
+
+<style scoped>
+.nav-tabs .nav-link {
+  color: #388E3C ;
+}
+.nav-tabs .nav-link.active {
+  color: #212121 ;
+}
+</style>

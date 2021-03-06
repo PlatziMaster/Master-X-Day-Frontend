895912c88744ab08649681648567a73df711b7c6
Andres Fernando <andres.fernando08@gmail.com>
2021-03-06 15:10:40 -0500

    [add] css file for card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Card.module.css';
 
 function Card({ children }) 
 {
   return (
-    <div className="Card">
+    <div className={styles.Card}>
       { children }
     </div>
   );

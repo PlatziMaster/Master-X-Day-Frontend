2c80af9971ceab9553f52233f3cbac1eaaccda3a
Adrian Bautista <adrianbautistaorozco@gmail.com>
2021-03-06 13:43:38 -0500

    Footer structure added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/scss/global.scss'
 import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+
+      <Footer />
     </div>
   );
 }

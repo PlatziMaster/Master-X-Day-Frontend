89dd9aa78c52ca71c7ad6053dec54f4d04db8874
Antonio <aguzmanv09@gmail.com>
2021-03-06 16:44:23 -0600

    Finish the connection to API

--- src/containers/Layout/index.jsx
+++ src/containers/Layout/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import useBoardLists from "../../hooks/useBoardLists";
 
 import "./Layout.css";
 
 const Layout = () => {
+  const { lists } = useBoardLists({ id: 'LOmGJfmG' });
+  console.log(lists);
+
   return (
     <div className="container">
       <h1>Hello World!</h1>
--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Layout from "../containers/Layout/";
+import Auth from "../containers/auth";
+import { useStateValue } from "../context";
+import { getToken } from "../context/actions";
 
 const App = () => {
+  const { token, dispatch } = useStateValue();
+
+  useEffect(() => {
+    if (!token) {
+      getToken({ dispatch });
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Layout} />
+        <Route exact path="/" component={Layout} />
+        <Route exact path="/auth" component={Auth} />
       </Switch>
     </BrowserRouter>
   );

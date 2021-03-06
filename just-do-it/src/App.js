4adb7ca89b6a47b7a04b38475bada4093bd51790
Santiago Guáqueta <santigeek@hotmail.com>
2021-03-06 13:58:37 -0500

    Adding branch components

--- just-do-it/src/App.js
+++ just-do-it/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello Team 11</p>
         <a
           className="App-link"
           href="https://reactjs.org"

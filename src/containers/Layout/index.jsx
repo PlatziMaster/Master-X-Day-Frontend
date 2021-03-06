6dd4de840b88eaa0f68620197b2896c206f69083
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-03-06 11:58:36 -0700

    Setting up the initial layout

---
+++ src/containers/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Layout.css";
+
+const Layout = () => {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default Layout;

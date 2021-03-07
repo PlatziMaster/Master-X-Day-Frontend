bd0ef1e9063a654fb5ffec593e1c9a81f06d3661
Sergio Gonzalez <sergioleonardo.gonzalez@gmail.com>
2021-03-06 21:27:04 -0500

    Api and deployment

--- src/components/CardPercentage.js
+++ src/components/CardPercentage.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "../styles/box.css"
-const CardPercentage = () => {
+const CardPercentage = (props) => {
+
+    const completed = props.completed
+
     return (
         <div className="containerBox">
         <div className="Box blue">
             <small className="Box__head">Completed vs Total</small>
-            <p className="Box__Number">50%</p>
+            <p className="Box__Number">{`${completed} %`}</p>
         </div>
  
         <div className="Box green">

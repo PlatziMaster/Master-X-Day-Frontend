ca533b9b5f09842ec908f2c31aae572f6397ed19
Felipe Duitama <felipe.duitama@3dves.com>
2021-03-06 19:04:16 -0500

    add team to home

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -1,10 +1,12 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { testAPI } from '../api';
 import Column from '../components/column';
+import Team from '../components/team';
 
 @Component({
   components: {
     Column,
+    Team,
   },
 })
 

a7b2b282f9b4ac1dd6703d20ecf9409230c88106
Adrian Bautista <adrianbautistaorozco@gmail.com>
2021-03-06 20:26:22 -0500

    Dashboard main page

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import DashboardItem from '../components/DashboardItem'
 
 // const key = 'a9a2107d1296e0416a93bddf26491c4a'
 const key = '93e0e4c144003bf990231de2aab641ee'
@@ -37,15 +38,19 @@ export default function Dashboard(props) {
     }, [1])
 
     return (
-        <div>
-            DASBOARD
+        <div className="container">
+            <h1 className="text-white">Interface</h1>
+            <div className="row">
+            {boards.length >= 1 ? (
+                boards.map(board => <DashboardItem item={board} key={board.id} />)
+            ) : null}
+            </div>
+            
             {/* {members.length >=1 ? (
                 members.map(member => <p>{member.username}</p>)
             ) : null} */}
 
-            {boards.length >= 1 ? (
-                boards.map(board => <Link to={`/dashboard/${board.id}`} key={board.id}>{board.name}</Link>)
-            ) : null}
+           
 
 
             

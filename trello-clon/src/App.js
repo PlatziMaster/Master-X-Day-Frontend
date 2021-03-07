1516ec0fea5ef7fcac8bae70c188a35dea1284fe
davidnherran <davidnherran@gmail.com>
2021-03-06 19:20:39 -0500

    Create custom css By Luis Enrique Lozada

--- trello-clon/src/App.js
+++ trello-clon/src/App.js
@@ -1,34 +1,52 @@
-import Card from './components/card'
-import menu from './icons/app-menu.svg'
-import home from './icons/home.svg'
-import table from './icons/table.svg'
-import dashb from './icons/dashb.svg'
-import star from './icons/star.svg'
-import lock from './icons/lock.svg'
-import "./App.css"
+import Card from "./components/card";
+import menu from "./icons/app-menu.svg";
+import home from "./icons/home.svg";
+import table from "./icons/table.svg";
+import dashb from "./icons/dashb.svg";
+import star from "./icons/star.svg";
+import lock from "./icons/lock.svg";
 
 function App() {
   return (
-    <div className="App">{/*Dont delete*/}
-      <div className="navbar bg-primary" >
+    <div className="App">
+      {/*Dont delete*/}
+      <div className="navbar bg-primary">
         <div className="container-fluid">
-          <button className="btn btn-light aa"><img src={menu} alt="Menu Icon"/></button>
-          <button className="btn btn-light"><img src={home} alt="Home Icon"/></button>
-          <button className="btn btn-light"><img src={table} alt="Table Icon"/></button>
-          </div>
+          <button className="btn btn-light">
+            <img src={menu} alt="Menu Icon" />
+          </button>
+          <button className="btn btn-light">
+            <img src={home} alt="Home Icon" />
+          </button>
+          <button className="btn btn-light">
+            <img src={table} alt="Table Icon" />
+          </button>
         </div>
+      </div>
       <div className="container-fluid">
         <div className="navbar">
-          <button className="btn btn-light"><img src={dashb} alt="Dashboard Icon"/>Board</button>
-          <h3 className="aa">Board Titles</h3>
-          <button className="btn btn-light"><img src={star} alt="Star Icon"/></button>
-          <button className="btn btn-light"><img src={lock} alt="Lock Icon"/>Private</button>
-          <button className="btn btn-light"><h4>Invite</h4></button>
+          <div className="row">
+            <button className="btn btn-light">
+              <img src={dashb} alt="Dashboard Icon" />
+              Board
+            </button>
+            <h1>Trello Clone</h1>
+            <button className="btn btn-light ml-4">
+              <img src={star} alt="Star Icon" />
+            </button>
+            <button className="btn btn-light ml-4">
+              <img src={lock} alt="Lock Icon" />
+              Private
+            </button>
+            <button className="btn btn-light ml-4">
+              <h4>Invite</h4>
+            </button>
+          </div>
         </div>
         <div className="row">
-            <Card title="To Do"/>
-            <Card title="In progress"/>
-            <Card title="Done"/>
+          <Card title="To Do" color="rgb(255, 238, 0)" />
+          <Card title="In progress" color="rgb(248, 120, 0)" />
+          <Card title="Done" color="rgb(248, 0, 0)" />
         </div>
       </div>
     </div>
--- trello-clon/src/components/card.jsx
+++ trello-clon/src/components/card.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Card = (props) => {
   return (
   <div className="col-lg-4">  
-    <div className="card bg-secondary">
-      <div className="card-header">
-        <h2 className="card-title">{props.title}</h2>
+    <div className="card background-card" style={{borderTop: `4px solid ${props.color}`}}>
+      <div className="card-header" >
+        <h2 className="card-title" >{props.title}</h2>
       </div>
-      <div className="card-body">
+      <div className="card-body card-border">
         <div className="row">
           <div className="col-md-12">
-            <div className="card bg-warning">
+            <div className="card background-p">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sit, iste vero repudiandae eveniet perferendis aliquam magni asperiores eligendi molestiae illum ipsum. Eaque quidem dicta eveniet at reiciendis assumenda explicabo.</p>
             </div>
           </div>
           <div className="col-md-12">
-            <div className="card bg-warning">
+            <div className="card background-p">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sit, iste vero repudiandae eveniet perferendis aliquam magni asperiores eligendi molestiae illum ipsum. Eaque quidem dicta eveniet at reiciendis assumenda explicabo.</p>
             </div>
           </div>

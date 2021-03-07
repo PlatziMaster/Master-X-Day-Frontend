10334fab7511e9e70468a3cda7349cb7aeb72549
Sergio Gonzalez <sergioleonardo.gonzalez@gmail.com>
2021-03-06 19:20:45 -0500

    Connection to API - Lists

--- platzi-trello-stadistics/src/App.js
+++ platzi-trello-stadistics/src/App.js
@@ -7,17 +7,31 @@ import PieChart from "./components/PieChart";
 
 const App = () => {
 
-    const getList = () => {
-        fetch("https://api.trello.com/1/boards/6043df7d663af602cf77c881?key=3ecdfc55093cc26752e7056b0834e1e4&token=57a87b374c6043be18dbc60f00962ba3a650c731b54df7f86b83e967becf22ec")
+    const KEY_API = "3ecdfc55093cc26752e7056b0834e1e4"
+    const TOKEN_API = "57a87b374c6043be18dbc60f00962ba3a650c731b54df7f86b83e967becf22ec"
+    const idBoard = "6043df7d663af602cf77c881"
+    const idListTodo = "6043df7d663af602cf77c882"
+    const idListDone = "6043df7d663af602cf77c884"
+    const idListProgress = "6043df7d663af602cf77c883"
+
+    const getList = IDList => {
+        const urlTodo = `https://api.trello.com/1/lists/${IDList}/cards?key=${KEY_API}&token=${TOKEN_API}`
+        fetch(urlTodo)
             .then((res)=>res.ok? Promise.resolve(res) : Promise.reject(res))
             .then((res)=>res.json())
             .then((res) => console.log(res))
     }
 
-    getList();
+    /*const getCardsOfLists = () => {
+        fetch(`https://api.trello.com/1/boards/${idBoard}/lists?key=${KEY_API}&token=${TOKEN_API}`)
+            .then((res)=>res.ok? Promise.resolve(res) : Promise.reject(res))
+            .then((res)=>res.json())
+            .then((res) => console.log(res))
+    }*/
+
+    getList(idListDone)
 
     return (
-    
       <div className="Dashboard">
         <div className="Dashboard__header">
           <Header />

195c2e931e3da6edd445f74287d503fd1ae99283
Jose Somilleda <joseluissom004@gmail.com>
2021-03-06 19:11:07 -0600

    Changes in App

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -6,11 +6,7 @@ import { getToken } from "../context/actions";
 import OnBoarding from "../containers/Onboarding/";
 import Dashboard from "../containers/Dashboard/";
 import Login from "../containers/Login/";
-<<<<<<< HEAD
-import BoardConfirm from "../containers/BoardConfirm";
-=======
 import Staticts from "../containers/Staticts";
->>>>>>> 14685be6a4e0b39a7a45ad7d0572403cb6892402
 
 const App = () => {
   const { token, dispatch } = useStateValue();
@@ -28,11 +24,7 @@ const App = () => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/onboarding" component={OnBoarding} />
-<<<<<<< HEAD
         <Route exact path="/boardconfirm" component={BoardConfirm}/>
-=======
-        <Route exact path="/statics" component={Staticts} />
->>>>>>> 14685be6a4e0b39a7a45ad7d0572403cb6892402
         <Route path="/" component={Login} />
       </Switch>
     </BrowserRouter>

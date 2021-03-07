7db9d67db02797e1e96b7fda5ddc85c5c26b2da6
Andres Fernando <andres.fernando08@gmail.com>
2021-03-06 19:07:59 -0500

    linked cards container to the app

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import OnBoarding from "../containers/Onboarding/";
 import Dashboard from "../containers/Dashboard/";
 import Login from "../containers/Login/";
+import Cards from "../containers/Cards/Cards";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/onboarding" component={OnBoarding} />
+        <Route exact path="/cards/:idList" component={Cards} />
         <Route path="/" component={Login} />
       </Switch>
     </BrowserRouter>
